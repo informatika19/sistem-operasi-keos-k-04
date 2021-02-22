a94c49f18b48a078e20973883a6c78435b5ca584
Stefanus1235 <stefanusjeremy@gmail.com>
2021-02-23 04:38:46 +0700

    Fix main untuk pencobaan

--- kit-1/kernel.c
+++ kit-1/kernel.c
@@ -5,19 +5,24 @@ void readString(char *string);
 void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
 
 int main() {
-  readString(0x8000);
-  printString("Ich bin Invalido interrupto!");
+  //Test
+  char Test[3];
+  Test[0] = 'T';
+  Test[1] = 'H';
+  Test[2] = 'E';
+  printString(&Test);
+  
   makeInterrupt21();
   while (1);
 }
 
 void printString(char *string){
-  int ah = 0x0e*16*16; // interrupt 0x10, 0Eh: write string, code Ox0e dikali 16*16 
+  int ah = 0x0e*16*16; // 0Eh: write string pada interrupt 0x10, code Ox0e dikali 16*16 
                        // untuk setup AH
   int i = 0; //untuk iterate, berguna untuk offset memori
   while (*(string+i) != '\0'){ // while hingga akhir string
     int al = *(string+i); 
-    interrupt(0x10,ah+al,0,0,0); 
+    interrupt(0x10,ah+al,0,0,0); // Call interrupt
     i++; // iterate
   }
   interrupt(0x10,ah+13,0,0,0); //+13 untuk membaca 'enter'.

6314f178cf09d7a2b2760dd9549fa8791c9c7bb4
S. Jeremy A <stefanusjeremy@gmail.com>
2021-02-24 19:15:15 +0700

    perubahan terhadap printString

--- kit-1/kernel.c
+++ kit-1/kernel.c
@@ -3,19 +3,20 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
 void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+void asciiPrint();
 
 int main() {
-  //Test
   char Test[30];
-  Test[0] = 'T';
-  Test[1] = 'H';
-  Test[2] = 'E';
-  printString(&Test);
+  makeInterrupt21();
 
+  //asciiPrint();
+  printString("Your name:");
   readString(&Test);
+
+  printString("\n");
+  printString("Selamat datang, ");
   printString(&Test);
   
-  makeInterrupt21();
   while (1);
 }
 
@@ -25,23 +26,13 @@ void printString(char *string){
   int i = 0; //untuk iterate, berguna untuk offset memori
   while (*(string+i) != '\0'){ // while hingga akhir string
     int al = *(string+i); 
+    if (*(string+i) == '\n'){
+      interrupt(0x10,ah+13,0,0,0); // 13 = ASCII untuk CR / enter
+    }
     interrupt(0x10,ah+al,0,0,0); // Call interrupt
     i++; // iterate
   }
-  interrupt(0x10,ah+13,0,0,0); //+13 untuk membaca 'enter'.
 }
-
-// void readString(char* string){
-//   int ah= 0*16*16; // AH di set 00 untuk read character.
-//   int i = 0;
-//   int al;
-//   while(*(string+i) != 13){ //Input bukan enter
-//     al = *(string+i);
-//     interrupt(0x16, ah+al, 0, 0, 0);
-//     i++; //iterate
-//   }
-//   interrupt(0x16, ah+al, 0, 0, 0);
-// }
 
 void readString(char *string) {
   char *cc = string;
@@ -64,4 +55,13 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX){
       printString("Invalid interrupt");
   }
 }
+
+// void asciiPrint() {
+//   printString("╔╗────╔═══╦═══╗\n");
+//   printString("║║────║╔═╗║╔═╗║\n");
+//   printString("║║╔╦══╣║─║║╚══╗\n");
+//   printString("║╚╝╣║═╣║─║╠══╗║\n");
+//   printString("║╔╗╣║═╣╚═╝║╚═╝║\n");
+//   printString("╚╝╚╩══╩═══╩═══╝\n");
+// }
 
